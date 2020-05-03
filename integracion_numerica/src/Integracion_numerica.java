d8f9bfd047ad6525c3decb6f028209ef5791328f
Antonio colli <36493960+antonioCC@users.noreply.github.com>
2020-05-02 21:23:24 -0500

    Add files via upload
    equipo:
    antonio contreras colli
    felipe balam canul

---
+++ integracion_numerica/src/Integracion_numerica.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author amton
+ */
+public class Integracion_numerica {
+
+    public static void main(String[] args) {
+        frmPrincipal a =new frmPrincipal();
+        a.setVisible(true);
+    }
+    
+}
